e8678b1f4343eca05587b0b5487777f9f98db03d
Petro Sahal(hhgtl) <petrosahal66@gmail.com>
2025-02-20 18:54:45 +0200

    [FEAT] Add localStorage in redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,7 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import {counterReducer} from "../model/counterReducer";
+import {loadState, saveState} from "../model/localStorage";
+import {throttle} from "lodash";
 
 const rootReducer = combineReducers({
     counter: counterReducer,
@@ -8,6 +10,7 @@ const rootReducer = combineReducers({
 // создание store
 export const store = configureStore({
     reducer: rootReducer,
+    preloadedState: loadState()
 })
 
 // автоматическое определение типа всего объекта состояния
@@ -18,3 +21,10 @@ export type AppDispatch = typeof store.dispatch
 // для возможности обращения к store в консоли браузера
 // @ts-ignore
 window.store = store
+
+
+store.subscribe(throttle(() => {
+    saveState({
+        counter: store.getState().counter
+    });
+}, 1000));
